d32c8e863251d58f042dda6b25fbdb1eaa0f361f
daiba <badaibou5@gmail.com>
2023-11-27 11:24:01 +0000

    first commit

--- mutability.js
+++ mutability.js
@@ -1,14 +1,5 @@
-const person = {
-    name: 'Rick',
-    age: 77,
-    country: 'US',
-  }
-
 const clone1 = Object.assign({}, person)
 const clone2 = Object.assign({}, person)
 const samePerson = person
 samePerson.age++
 samePerson.country = 'FR'
-console.log(clone1)
-console.log(clone2)
-console.log(samePerson)
