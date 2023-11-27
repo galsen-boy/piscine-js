f5fd488dbcb7ab416a8027d67677297621d2f892
daiba <badaibou5@gmail.com>
2023-11-27 12:32:04 +0000

    first commit

--- concat-str.js
+++ concat-str.js
@@ -1,5 +1,3 @@
-const concatStr = (n1, n2) => {
-    return n1.concat(n2)
+const concatStr = (a, b) => {
+    return a+''+b
 }
-console.log(concatStr([1,2,3], [4,5,6]))
-console.log(concatStr('daibbou', 'ba'))
