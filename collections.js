ac2efb6d1a345ee68007a26e9a0d393eb2293585
daiba <badaibou5@gmail.com>
2023-11-28 13:24:46 +0000

    js commit

--- collections.js
+++ collections.js
@@ -23,7 +23,7 @@ const objToArr = (obj) => {
     return Object.values(obj)
 }
 const objToMap = (obj) => {
-    return new Map(objToArr(obj))
+    return Object.entries(obj)
 }
 const arrToObj = (arr) => {
     return Object.fromEntries(arr)
