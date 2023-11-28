16cee8ebf24efec0030ab37fe6675b6ce15c7dee
daiba <badaibou5@gmail.com>
2023-11-28 11:11:10 +0000

    js commit

--- physics.js
+++ physics.js
@@ -1,13 +1,12 @@
-  const getAcceleration = (object) => {
-    if (object.hasOwnProperty('f') && object.hasOwnProperty('m')) {
-        return object.f / object.m
-    } else if (object.hasOwnProperty('Δv') && object.hasOwnProperty('Δt')) {
-        return object.Δv / object.Δt
-    } else if (object.hasOwnProperty('t') && object.hasOwnProperty('d')) {
-        return 2*object.t /(object.d/10)**2
+  const getAcceleration = (obj) => {
+    if (obj.hasOwnProperty('f') && obj.hasOwnProperty('m')) {
+        return obj.f / obj.m
+    } else if (obj.hasOwnProperty('Δv') && obj.hasOwnProperty('Δt')) {
+        return obj.Δv / obj.Δt
+    } else if (obj.hasOwnProperty('t') && obj.hasOwnProperty('d')) {
+        return 2*obj.t /(obj.d/10)**2
 
     } else {
         return "impossible"
     }
   }
-  console.log(getAcceleration(object))
