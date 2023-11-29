6e0585a041d6395f9c0678f4cf19bc7fe11ca535
daiba <badaibou5@gmail.com>
2023-11-29 16:41:02 +0000

    js commit

--- index-of.js
+++ index-of.js
@@ -1,9 +1,38 @@
-const indexÀf = (index) => {
-    
-}
-const lastIndexOf = () => {
+function indexOf(array, value, index) {
+    if (!index) {
+        index = 0
+    }
 
+    for (let i = index; i < array.length; i++) {
+        if (array[i] === value) {
+            return i
+        }
+    }
+    return -1
 }
-const includes = () => {
-    
+
+function lastIndexOf(array, value, index) {
+    if (!index) {
+        index = array.length - 1
+    }
+
+    for (let i = index; i >= 0; i--) {
+        if (array[i] === value) {
+            return i
+        }
+    }
+    return -1
+}
+
+function includes(array, value, index) {
+    if (!index) {
+        index = 0
+    }
+
+    for (let i = index; i < array.length; i++) {
+        if (array[i] === value) {
+            return true
+        }
+    }
+    return false
 }
