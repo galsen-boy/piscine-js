28d54d7618d9471d522616c7a34e8e78aafc8206
daiba <badaibou5@gmail.com>
2023-11-30 17:11:02 +0000

    js commit

--- get.js
+++ get.js
@@ -1,5 +1,8 @@
-const get = (src, path) => {
+function get(src, path) {
     return path.split(".").reduce(function (obj, key) {
+        if (obj === undefined) {
+            return undefined;
+        }
         return obj[key];
     }, src);
 }
