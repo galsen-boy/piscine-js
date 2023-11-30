e09beca48e6de9acbb1cc2584dace6c2e1007933
daiba <badaibou5@gmail.com>
2023-11-30 16:30:50 +0000

    js commit

---
+++ molecules-cells.js
@@ -0,0 +1,30 @@
+const RNA = (adnstr) => {
+    let res = ''
+    for(let i = 0;i<adnstr.length;i++){
+        if(adnstr[i] === 'A'){
+            res += 'U'
+        }else if(adnstr[i] === 'C'){
+            res += 'G'
+        }else if(adnstr[i] === 'G'){
+            res += 'C'
+        }else if(adnstr[i] === 'U'){
+            res += 'A'
+        }
+    }
+    return res
+}
+const DNA = (adnstr) => {
+    let res = ''
+    for(let i = 0;i<adnstr.length;i++){
+        if(adnstr[i] === 'A'){
+            res += 'T'
+        }else if(adnstr[i] === 'C'){
+            res += 'G'
+        }else if(adnstr[i] === 'G'){
+            res += 'C'
+        }else if(adnstr[i] === 'T'){
+            res += 'A'
+        }
+    }
+    return res
+}
