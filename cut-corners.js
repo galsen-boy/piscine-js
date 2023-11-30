8d7d77fa01c7774701e0de075911103dbf067805
daiba <badaibou5@gmail.com>
2023-11-30 14:35:39 +0000

    js commit

--- cut-corners.js
+++ cut-corners.js
@@ -77,6 +77,8 @@ const trunc = (n) => {
 const modulo = (a, b) => {
     // check if a and b are negative
     const aSign = Math.sign(a);
+
+    //make a and b absolute values
     a = Math.abs(a);
     b = Math.abs(b);
     var res = 0;
@@ -85,7 +87,11 @@ const modulo = (a, b) => {
     while (i <= a) {
         i += b;
     }
+
+    //minus the last b to find the last multiple of b below a
     i -= b;
+
+    // the modulo is the difference between a and i
     res = a - i;
     if (aSign < 0) {
         res = -res;
