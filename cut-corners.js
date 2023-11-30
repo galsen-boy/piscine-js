49f2358419ebcf47ff0bb3013793e099080bc8c2
daiba <badaibou5@gmail.com>
2023-11-30 14:30:16 +0000

    js commit

--- cut-corners.js
+++ cut-corners.js
@@ -6,7 +6,7 @@ const round = (n) => {
         n = -n
         flag = true
     }
-    ost = n % 1
+    ost = modulo(n, 1)
     if (ost > 0.5) {
         res = n + 1 - ost
     } else {
@@ -20,7 +20,7 @@ const round = (n) => {
 const ceil = (n) => {
     let ost
     let res
-    ost = n % 1
+    ost = modulo(n, 1)
     if (n > 0 && ost != 0) {
         return res = n + 1 - ost
     } else {
@@ -31,7 +31,7 @@ const ceil = (n) => {
 const floor = (n) => {
     let ost
     let res
-    ost = n % 1
+    ost = modulo(n, 1)
     if (n > 0 && ost != 0) {
         return res = n - ost
     } else if (ost === 0) {
@@ -44,10 +44,10 @@ const floor = (n) => {
     }
     return n
 }
-function trunc(n) {
+const trunc = (n) => {
     let ost
     let res
-    ost = n % 1
+    ost = modulo(n, 1)
     if (n > 0 && ost != 0) {
         return res = n - ost
     } else {
@@ -56,4 +56,53 @@ function trunc(n) {
         return res = n - ost
     }
     return n
+}
+
+const multiply = (a, b) => {
+    let result = 0;
+    let flag = false
+    if (b < 0) {
+        b = -b
+        flag = true
+    }
+    while (b > 0) {
+        result += a
+        b--
+    }
+    if (flag) {
+        result = -result
+    }
+    return result
+}
+
+const divide = (a, b) => {
+    let count = 0
+    let flag = false
+    if (a < 0 && b < 0) {
+        a = -a
+        b = -b
+    }
+
+    if (a < 0) {
+        a = -a
+        flag = true
+    }
+    if (b < 0) {
+        b = -b
+        flag = true
+    }
+    let result = a
+    while (result >= b) {
+        result -= b
+        count++
+    }
+    if (flag) {
+        count = -count
+    }
+    return count
+}
+const modulo = (a, b) => {
+    const quotient = divide(a, b);
+    const remainder = a - multiply(b, quotient)
+    return remainder;
 }
