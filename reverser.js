ab4531250909c78596a2fddd64af7492cc129d81
badaibou5@gmail.com <badaibou5@gmail.com>
2023-11-30 02:20:57 +0100

    js commit

---
+++ reverser.js
@@ -0,0 +1,12 @@
+const reverser = (arr) => {
+    let arrf = ''
+    for (let i = arr.length-1 ; i>= 0; i--) {
+        arrf += arr[i]
+    }
+    if (typeof arrf === 'string') {
+        return arrf
+    }
+    if (Array.isArray(arrf)) {
+        return arrf.split()
+    }
+}
