650a7d24234211842618ed6eee229ca4015b0911
badaibou5@gmail.com <badaibou5@gmail.com>
2023-12-05 02:50:48 +0100

    js commit

--- date-is.js
+++ date-is.js
@@ -1,3 +1,4 @@
 const isValid = (date) => date > 0 || date < 0;
 const isAfter = (date1, date2) => date1 > date2;
 const isBefore = (date1, date2) => date1 < date2;
+const isFuture = (date) => date > Date.now()
