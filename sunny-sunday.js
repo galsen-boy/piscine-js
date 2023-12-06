7adb4ae1d303e48bf38ccaf97e4ad62a384522d1
badaibou5@gmail.com <badaibou5@gmail.com>
2023-12-06 05:12:58 +0100

    js commit

---
+++ sunny-sunday.js
@@ -0,0 +1,14 @@
+const sunnySunday = (date) => {
+    let weekDays = [
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ]
+    let first = new Date('0001-01-01')
+    let weekDayNum = date.getDay()
+    let week = Math.ceil(((date - first + 1) / 86400000) / 6);
+  return weekDays[weekDayNum]
+}
