9f73c875e0cd7682738556aa8bbc5fa14a47b921
daiba <badaibou5@gmail.com>
2023-12-08 20:49:01 -0500

    js commit

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -6,16 +6,16 @@ function pimp() {
     if (!button.classList.contains("unpimp")) {
         button.classList.add(styles[counter]);
         counter++;
-    // } else {
-    //     counter--;
-    //     button.classList.remove(styles[counter]);
-    //     if (counter === 0) {
-    //         button.classList.toggle("unpimp");
-    //     }
-    // }
-    // if (counter === styles.length) {
-    //     button.classList.toggle("unpimp");
-    // }
+    } else {
+        counter--;
+        button.classList.remove(styles[counter]);
+        if (counter === 0) {
+            button.classList.toggle("unpimp");
+        }
+    }
+    if (counter === styles.length) {
+        button.classList.toggle("unpimp");
+    }
 }
 
 export { pimp };
