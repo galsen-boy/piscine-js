1e00806412ef1f0833fb4ff181f693c8ad472337
galsen-boy <badaibou5@gmail.com>
2023-12-13 01:38:40 -0500

    js commit

--- neuron.js
+++ neuron.js
@@ -11,7 +11,7 @@ function neuron(arr) {
                 .toLowerCase();
             res['questions'][questionKey] ||= {};
             res['questions'][questionKey]['question'] = question;
-            res['questions'][questionKey]['responses'] ||= [];
+            res['questions'][questionKey]['responses'] ||= []
             res['questions'][questionKey]['responses'].push(response)
         } else if (/orders:/i.test(str[0])) {
             let [order, response] = parseQuestionOrder(str)
