45a418cd2bc88e8ac95df9b1ae08d3911d7e17d6
daiba <badaibou5@gmail.com>
2023-12-19 04:07:57 +0000

    js commit

--- gatecrashers.mjs
+++ gatecrashers.mjs
@@ -1,53 +1,78 @@
 import http from 'http';
-import fs from 'fs/promises';
-import path from 'path';
-import url from 'url';
+import { join } from 'path';
+import { readFile, writeFile } from 'fs/promises';
+
+const authorizedUsers = {
+  'Caleb_Squires': 'abracadabra',
+  'Tyrique_Dalton': 'abracadabra',
+  'Rahima_Young': 'abracadabra',
+};
+
+const directoryPath = 'guests';
+
+function handlePostRequest(request, response) {
+  let body = '';
+  request.on('data', chunk => {
+    body += chunk.toString();
+  });
+
+  request.on('end', async () => {
+    try {
+      const { username, password } = request.auth;
+      if (!authorizedUsers[username] || authorizedUsers[username] !== password) {
+        response.statusCode = 401;
+        response.setHeader('Content-Type', 'application/json');
+        response.end(JSON.stringify({ error: 'Authorization Required' }));
+        return;
+      }
+
+      const guestName = request.url.slice(1);
+      const filePath = join(directoryPath, `${guestName}.json`);
+      const guestData = JSON.parse(body);
+
+      await writeFile(filePath, JSON.stringify(guestData));
+
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'application/json');
+      response.end(JSON.stringify(guestData));
+    } catch (error) {
+      response.statusCode = 500;
+      response.setHeader('Content-Type', 'application/json');
+      response.end(JSON.stringify({ error: 'Server failed' }));
+      console.error(error);
+    }
+  });
+}
+
+function handleRequest(request, response) {
+  const { url, method, headers } = request;
+
+  if (method === 'POST') {
+    if (!headers.authorization) {
+      response.statusCode = 401;
+      response.setHeader('WWW-Authenticate', 'Basic realm="Authorization Required"');
+      response.setHeader('Content-Type', 'application/json');
+      response.end(JSON.stringify({ error: 'Authorization Required' }));
+      return;
+    }
+
+    const auth = headers.authorization.replace(/^Basic /, '');
+    const [username, password] = Buffer.from(auth, 'base64').toString().split(':');
+    request.auth = { username, password };
+
+    handlePostRequest(request, response);
+  } else {
+    response.statusCode = 404;
+    response.setHeader('Content-Type', 'application/json');
+    response.end(JSON.stringify({ error: 'Guest not found' }));
+  }
+}
+
+const server = http.createServer(handleRequest);
 
 const port = 5000;
-const directoryPath = 'guests';
-const authorizedUsers = ['Caleb_Squires', 'Tyrique_Dalton', 'Rahima_Young'];
-const password = 'abracadabra';
-
-const server = http.createServer(async (req, res) => {
-  const parsedUrl = url.parse(req.url, true);
-  const pathname = parsedUrl.pathname;
-  const query = parsedUrl.query;
-
-  if (req.method === 'POST' && authorizedUsers.includes(req.headers.authorization)) {
-    const guestName = pathname.slice(1);
-    const filePath = path.join(directoryPath, `${guestName}.json`);
-    const fileData = await fs.readFile(filePath);
-    const guest = JSON.parse(fileData);
-
-    if (query.plusOne) {
-      guest.plusOnes = (guest.plusOnes || []).concat(query.plusOne);
-      await fs.writeFile(filePath, JSON.stringify(guest));
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(guest));
-    } else {
-      res.writeHead(400, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ error: 'missing plusOne parameter' }));
-    }
-  } else if (req.method === 'GET' && authorizedUsers.includes(req.headers.authorization)) {
-    try {
-      const guest = await fs.readFile(path.join(directoryPath, pathname.slice(1)), 'utf8');
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(guest);
-    } catch (error) {
-      if (error.code === 'ENOENT') {
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ error: 'guest not found' }));
-      } else {
-        res.writeHead(500, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ error: 'server failed' }));
-      }
-    }
-  } else {
-    res.writeHead(401, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ error: 'unauthorized' }));
-  }
-});
-
 server.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
+
+export default server;
