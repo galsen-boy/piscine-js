ed1d332e43bffed86cd9eda74ea2cd72e84db59b
daiba <badaibou5@gmail.com>
2023-12-19 04:56:49 +0000

    js commit

--- gatecrashers.mjs
+++ gatecrashers.mjs
@@ -1,21 +1,4 @@
-/*
-To modify the guest list now your friends will need to make authenticated requests. 
-You decided that only your 3 best friends 
-(Caleb_Squires, Tyrique_Dalton and Rahima_Young) 
-will be able to modify the list. You also told them the secret 
-password abracadabra in order to do that.
 
-The request query will look like this curl -u Caleb_Squires:abracadabra ....
-
-The server should properly handle unauthorized requests using the error code 401.
-
-Apart for the authentication part your server's behavior should remain unchanged from uninvited exercise:
-
-You will listen to port 5000 and print a message containing the port you are using.
-For each authorized http POST request, your program 
-should create the corresponding JSON file and store the contents of the body, and 
-then provide the content as JSON in the HTTP response, if possible.
-*/
 
 import http from "http";
 import { readFile, writeFile } from "fs/promises";
@@ -40,7 +23,6 @@ const guestData = (req, res) => {
             .end(bodyRes);
     }
 
-    //reading credentials
 
     let baseAuthorusation = req.headers['authorization'];
     if (!baseAuthorusation) {
@@ -54,17 +36,7 @@ const guestData = (req, res) => {
         return;
     }
 
-    // in the test for this task they put body in the headers !!!!!
     let bodyReq = req.headers['body'];
-    // // reading the request body
-    // let bodyReq = [];
-    // req.on('data', (chunk) => {
-    //     bodyReq.push(chunk);
-    // }).on('end', () => {
-    //     bodyReq = Buffer.concat(bodyReq).toString();
-    //     // at this point, `body` has the entire request body stored in it as a string
-    // }).on('error', errHandler);
-
     writeFile(`${pathGuests}/${guestFile}`, bodyReq)
         .then(() => {
             let bodyRes = bodyReq;
